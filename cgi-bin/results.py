1c24defc6915dfa2c98e2021d5b5b39055b11e4d
Samy-sama <Samy.zarour@mail.mcgill.ca>
2016-10-31 15:38:03 -0400

    made json parsing work and also added a tentative ajax snippet

--- cgi-bin/results.py
+++ cgi-bin/results.py
@@ -2,12 +2,21 @@
 # -*- coding: UTF-8 -*-
 
 # Import modules for CGI handling 
-import cgi, cgitb
+import sys, json, cgi
 
 form = cgi.FieldStorage()
 userName = "blank"
 userName = form["userName"].value
 
-print "Content-type:text/html\r\n\r\n"
-print userName
+result = {'success':'true','message':'The Command Completed Successfully'};
+inputedTest = {'Name':'test'}
 
+myjson = json.loads('["foo", {"bar":["baz", null, 1.0, 2]}]')
+# Do something with 'myjson' object
+
+#print 'Content-Type: application/json\n\n'
+
+
+print "Content-type:text/html\r\n\r\n"
+print json.dumps(result)
+#print "test"
